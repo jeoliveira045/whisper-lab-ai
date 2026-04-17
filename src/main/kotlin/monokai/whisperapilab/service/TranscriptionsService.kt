cce53a1ea04adcfdf1b6b844b48f92666de912ac
Jean Mateus Vasconcelos de Oliveira <jean.mateus.1997@gmail.com>
2026-04-17 00:23:08 -0300

    Adding comments

--- src/main/kotlin/monokai/whisperapilab/service/TranscriptionsService.kt
+++ src/main/kotlin/monokai/whisperapilab/service/TranscriptionsService.kt
@@ -27,30 +27,51 @@ class TranscriptionsService(
 
 
     fun transcription(arquivo: MultipartFile): String = runBlocking {
+        /**
+         * Quebrar o arquivo em partes menores para serem processados pela API
+         */
         val chunks = mediaProcessor.splitFile(arquivo, CHUNK_DURATION_SECONDS)
-        
+
+        /**
+         * Aqui é realizado o processamento das partes pela API. Primeiro,
+         * um mapa indexado das partes é criado, para então serem processados de forma
+         * assincrona pelo sistema pelo metodo async. De forma paralela, todos os asyncs
+         * iniciados retornarão um Pair com o index da parte e a transcrição. O metodo awaitAll()
+         * obriga o programa a esperar todos os processamentos serem finalizados para então da
+         * sequência as outras instruções
+         */
+
         val results = chunks.mapIndexed { index, chunk ->
             async(Dispatchers.IO) {
                 println("Processando parte ${index + 1} de ${chunks.size}")
                 index to transcribeChunk(chunk)
             }
         }.awaitAll()
-        
+
+
         results.sortedBy { it.first }
             .joinToString(" ") { it.second }
             .trim()
     }
     
     private fun transcribeChunk(arquivo: MultipartFile): String {
+        /**
+         * Cada uma das partes é processada aqui
+         */
         val latch = CountDownLatch(1)
         val resultBuilder = StringBuilder()
 
         SpeechClient.create().use { speechClient ->
+            //1. O audio é convertido para ByteArray para poder ser executado em tempo de execução
             val audioBytes = audioConversionContext.convertAudio(arquivo)
+            //2. Configura os parametros do transcritor. Linguagem, rate de frequencia e encoding são configurados aqui
             val streamingConfig = speechConfigFactory.createDefaultStreamingConfig()
-            
+            //3. Aqui é configurado a transcrição sera armazenada. Isso é necessario pois o sistema realiza a transcrição aos poucos(streaming)
+            // e por isso ela é armazenada aos poucos(StringBuilder). Sem isso a transcrição pode ser feita de forma repetitiva, o que geraria strings muito grandes para serem processadas
             val responseObserver = TranscriptionResponseObserver(latch, resultBuilder)
+            //4. Aqui é o metodo que irá performar a transcrição de forma bidirecional: É enviado o audio enquanto se recebe a transcrição do mesmo
             val callable = speechClient.streamingRecognizeCallable()
+            //5. O utilitario do sistema que aplica as configurações e executa o processo de transcrição.
             val streamHandler = AudioStreamHandler(callable, responseObserver)
             
             streamHandler.sendConfig(streamingConfig)
